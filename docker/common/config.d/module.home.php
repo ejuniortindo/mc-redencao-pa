63cd454067fb9d334b8401dd42baa8cf9ba314d5
Edson Junior <ejr-94@outlook.com.br>
2026-04-30 11:29:38 -0300

    Atualização

--- docker/common/config.d/module.home.php
+++ docker/common/config.d/module.home.php
@@ -2,11 +2,11 @@
 return [
     "module.home" => [
         'home-header' => env('MODULE_HOME_IMAGE_HEADER','img/home/home-header/home-header.jpg'),
-        'home-agents' => env('MODULE_HOME_IMAGE_AGENT','img/home/home-entities/agents.jpg'),
-        'home-events' => env('MODULE_HOME_IMAGE_EVENTS','img/home/home-entities/events.jpg'),
-        'home-opportunities' => env('MODULE_HOME_IMAGE_OPPORTUNITIES','img/home/home-entities/opportunities.jpg'),
-        'home-projects' => env('MODULE_HOME_IMAGE_PROJECTS','img/home/home-entities/projects.jpg'),
-        'home-spaces' => env('MODULE_HOME_IMAGE_SPACES','img/home/home-entities/spaces.jpg'),
+        'home-agents' => env('MODULE_HOME_IMAGE_AGENT','img/home/home-entities/agentes.jpeg'),
+        'home-events' => env('MODULE_HOME_IMAGE_EVENTS','img/home/home-entities/eventos.jpeg'),
+        'home-opportunities' => env('MODULE_HOME_IMAGE_OPPORTUNITIES','img/home/home-entities/oportunidades.jpeg'),
+        'home-projects' => env('MODULE_HOME_IMAGE_PROJECTS','img/home/home-entities/projetos.jpeg'),
+        'home-spaces' => env('MODULE_HOME_IMAGE_SPACES','img/home/home-entities/espacos.jpeg'),
         'home-register' => env('MODULE_HOME_IMAGE_REGISTER','img/home/home-register/background.jpg'),      
     ]    
 ];
