8b4131198b244ac90859fb8b5d118a58063cf060
Edson Junior <ejr-94@outlook.com.br>
2026-08-10 11:10:52 -0300

    Atualização da imagens dos banners e da home

--- docker/common/config.d/home-header.php
+++ docker/common/config.d/home-header.php
@@ -9,17 +9,17 @@ return [
     #'homeHeader.background' => 'img/home/home-header/home-header.jpg',
 
     /* Primeiro banner */
-    'homeHeader.banner' => 'img/banner-paulo-gustavo.png',
-    'homeHeader.bannerLink' => 'https://redencao.pa.gov.br/',        
-    'homeHeader.downloadableLink' => false,  /* Define se link é para download ou para abrir em uma nova aba */
+    'homeHeader.banner' => 'img/home/home-banners/banner-aldir-blanc.png',
+//    'homeHeader.bannerLink' => 'https://redencao.pa.gov.br/',
+//    'homeHeader.downloadableLink' => false,  /* Define se link é para download ou para abrir em uma nova aba */
 	
 	/* Segundo banner */
-    'homeHeader.secondBanner' => 'img/banner-paulo-gustavo.png',
-    'homeHeader.secondBannerLink' => 'https://redencao.pa.gov.br/',    
-    'homeHeader.secondDownloadableLink' => false,
+    'homeHeader.secondBanner' => 'img/home/home-banners/banner-forum-de-cultura.png',
+//    'homeHeader.secondBannerLink' => 'https://redencao.pa.gov.br/',
+//    'homeHeader.secondDownloadableLink' => false,
 
     /* Terceiro banner */
-    'homeHeader.thirdBanner' => 'img/banner-paulo-gustavo.png',
-    'homeHeader.thirdBannerLink' => 'https://redencao.pa.gov.br/',
+    'homeHeader.thirdBanner' => 'img/home/home-banners/banner-ouvidoria-da-cultura.png',
+    'homeHeader.thirdBannerLink' => 'https://falabr.cgu.gov.br/v2/?modoOuvidoria=1&ouvidoriaInterna=false',
     'homeHeader.thirdDownloadableLink' => false,
 ];
--- docker/common/config.d/module.home.php
+++ docker/common/config.d/module.home.php
@@ -2,11 +2,11 @@
 return [
     "module.home" => [
         'home-header' => env('MODULE_HOME_IMAGE_HEADER','img/home/home-header/home-header.jpg'),
-        'home-agents' => env('MODULE_HOME_IMAGE_AGENT','img/home/home-entities/agentes.jpeg'),
-        'home-events' => env('MODULE_HOME_IMAGE_EVENTS','img/home/home-entities/eventos.jpeg'),
-        'home-opportunities' => env('MODULE_HOME_IMAGE_OPPORTUNITIES','img/home/home-entities/oportunidades.jpeg'),
-        'home-projects' => env('MODULE_HOME_IMAGE_PROJECTS','img/home/home-entities/projetos.jpeg'),
-        'home-spaces' => env('MODULE_HOME_IMAGE_SPACES','img/home/home-entities/espacos.jpeg'),
+        'home-agents' => env('MODULE_HOME_IMAGE_AGENT','img/home/home-entities/agentes.png'),
+        'home-events' => env('MODULE_HOME_IMAGE_EVENTS','img/home/home-entities/eventos.png'),
+        'home-opportunities' => env('MODULE_HOME_IMAGE_OPPORTUNITIES','img/home/home-entities/oportunidades.png'),
+        'home-projects' => env('MODULE_HOME_IMAGE_PROJECTS','img/home/home-entities/projetos.png'),
+        'home-spaces' => env('MODULE_HOME_IMAGE_SPACES','img/home/home-entities/espacos.png'),
         'home-register' => env('MODULE_HOME_IMAGE_REGISTER','img/home/home-register/background.jpg'),      
     ]    
 ];
